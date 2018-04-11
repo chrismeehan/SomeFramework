7ee25eba36739c24b29f36133cba33e24df6e097
Chris Meehan <Chris.X.Meehan.-ND@disney.com>
2018-04-10 20:34:40 -0700

    Added framework stuff

---
+++ SomeFramework/SomeFramework/SomeFramework.h
@@ -0,0 +1,19 @@
+//
+//  SomeFramework.h
+//  SomeFramework
+//
+//  Created by Meehan, Chris X. -ND on 4/10/18.
+//  Copyright © 2018 Meehan, Chris X. -ND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SomeFramework.
+FOUNDATION_EXPORT double SomeFrameworkVersionNumber;
+
+//! Project version string for SomeFramework.
+FOUNDATION_EXPORT const unsigned char SomeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SomeFramework/PublicHeader.h>
+
+
---
+++ SomeFramework/SomeFramework/SomeFrameworkClass.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  SomeFramework
+//
+//  Created by Meehan, Chris X. -ND on 4/10/18.
+//  Copyright © 2018 Meehan, Chris X. -ND. All rights reserved.
+//
+
+import Foundation
+
+public class SomeFrameworkClass {
+    
+    
+    public static func getSomeString() -> String {
+        return "yoness"
+    }
+    
+}
